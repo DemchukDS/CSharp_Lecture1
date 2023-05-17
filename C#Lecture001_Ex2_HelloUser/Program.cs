db8d0e58d02e0fe89202e38e6e77f4136e7fa68f
DemchukDS <demchuk.btg@gmail.com>
2023-05-17 19:18:35 +0300

    Added symbol

--- C#Lecture001_Ex2_HelloUser/Program.cs
+++ C#Lecture001_Ex2_HelloUser/Program.cs
@@ -2,4 +2,4 @@
 string UserName = Console.ReadLine();
 Console.Write("Enter User Soname: ");
 string UserSoname = Console.ReadLine();
-Console.Write("Hello "); Console.Write(UserName); Console.Write(" "); Console.Write(UserSoname); Console.Write("!");
+Console.Write("Hello, "); Console.Write(UserName); Console.Write(" "); Console.Write(UserSoname); Console.Write("!");
