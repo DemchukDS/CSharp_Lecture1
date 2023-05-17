52953e5d9b168abeef74d03ed0c4faf4ddcdc2ce
DemchukDS <demchuk.btg@gmail.com>
2023-05-17 19:57:10 +0300

    Ex4

---
+++ C#Lecture001_Ex4_DivOf2Numb/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Enter the first number: ");
+double firstNumber = Convert.ToSingle(Console.ReadLine());
+Console.Write("Enter the second number: ");
+double secondNumber = Convert.ToSingle(Console.ReadLine());
+Console.Write("The summ you entered numbers, are ");
+Console.WriteLine(firstNumber / secondNumber);
