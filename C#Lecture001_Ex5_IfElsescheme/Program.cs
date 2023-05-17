16d6ff5e82146f20678a718c7225f3b20870a8b5
DemchukDS <demchuk.btg@gmail.com>
2023-05-17 20:26:09 +0300

    Names

---
+++ C#Lecture001_Ex5_IfElsescheme/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter a user name: ");
+string userName = Console.ReadLine();
+if (userName.ToLower() == "dima")
+{
+    Console.WriteLine("Hello my favorit person");
+}
+else
+{
+    Console.Write("Hello ");
+    Console.WriteLine(userName);
+}
