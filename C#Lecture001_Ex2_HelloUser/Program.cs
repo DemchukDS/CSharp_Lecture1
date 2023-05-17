beafd46177226072bb9704b723340ee69a412173
DemchukDS <demchuk.btg@gmail.com>
2023-05-17 20:29:39 +0300

    req

--- C#Lecture001_Ex2_HelloUser/Program.cs
+++ C#Lecture001_Ex2_HelloUser/Program.cs
@@ -2,4 +2,8 @@
 string UserName = Console.ReadLine();
 Console.Write("Enter User Soname: ");
 string UserSoname = Console.ReadLine();
-Console.Write("Hello, "); Console.Write(UserName); Console.Write(" "); Console.Write(UserSoname); Console.Write("!");
+Console.Write("Hello, "); 
+Console.Write(UserName); 
+Console.Write(" "); 
+Console.Write(UserSoname); 
+Console.Write("!");
