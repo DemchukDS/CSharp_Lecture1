3c65f7dc7ae92b69fc1278f47c126f79909adcd1
DemchukDS <demchuk.btg@gmail.com>
2023-05-17 20:43:07 +0300

    garbage

--- C#Lecture001_Ex2_HelloUser/Program.cs
+++ C#Lecture001_Ex2_HelloUser/Program.cs
@@ -1,9 +1,9 @@
-﻿Console.WriteLine("Enter User Name: ");
-string username = Console.ReadLine();
-Console.WriteLine("Enter User Soname: ");
-string usersoname = Console.ReadLine();
+﻿Console.Write("Enter User Name: ");
+string userName = Console.ReadLine();
+Console.Write("Enter User Soname: ");
+string userSoname = Console.ReadLine();
 Console.Write("Hello, "); 
-Console.Write(username); 
+Console.Write(userName); 
 Console.Write(" "); 
-Console.Write(usersoname); 
+Console.Write(userSoname); 
 Console.Write("!");
