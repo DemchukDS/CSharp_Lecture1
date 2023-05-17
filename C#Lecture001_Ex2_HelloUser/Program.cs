6713b4072b8cd56aac9ff8ee8cba2c57d9733700
DemchukDS <demchuk.btg@gmail.com>
2023-05-17 12:39:43 +0300

    Ex2 programm

---
+++ C#Lecture001_Ex2_HelloUser/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Enter User Name: ");
+string UserName = Console.ReadLine();
+Console.Write("Enter User Soname: ");
+string UserSoname = Console.ReadLine();
+Console.Write("Hello "); Console.Write(UserName); Console.Write(" "); Console.Write(UserSoname); Console.Write("!");
