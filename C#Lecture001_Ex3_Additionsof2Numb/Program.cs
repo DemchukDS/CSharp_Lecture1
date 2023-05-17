9d1aa1cb61d3b8c28d14bcf762f8b0de48092ff3
DemchukDS <demchuk.btg@gmail.com>
2023-05-17 20:09:44 +0300

    The sum rundom numbers

--- C#Lecture001_Ex3_Additionsof2Numb/Program.cs
+++ C#Lecture001_Ex3_Additionsof2Numb/Program.cs
@@ -1,4 +1,7 @@
 ﻿float firstNumber = new Random().Next(1,10);
+Console.Write(firstNumber);
 float secondNumber = new Random().Next(1,10);
-Console.Write("The summ rundom numbers, are ");
+Console.Write(" + ");
+Console.Write(secondNumber);
+Console.Write(" = ");
 Console.WriteLine(firstNumber + secondNumber);
