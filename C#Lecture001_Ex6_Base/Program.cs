2a7b3a34bbbfea36f5c1338dbdec1a1717d8f51e
DemchukDS <demchuk.btg@gmail.com>
2023-05-17 21:02:28 +0300

    Ex6

--- C#Lecture001_Ex6_Base/Program.cs
+++ C#Lecture001_Ex6_Base/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int obj1 , obj2 , obj3 , obj4 , obj5;
+int max = obj1;
+
+Cosole.WriteLine("Enter 5 object numbers: ");
+Cosole.Write("The first - ");
+Console.ReadLine(obj1);
+Cosole.Write("The second - ");
+Console.ReadLine(obj2);
+Cosole.Write("The third - ");
+Console.ReadLine(obj3);
+Cosole.Write("The fourth - ");
+Console.ReadLine(obj4);
+Cosole.Write("The fifth - ");
+Console.ReadLine(obj5);
+
+if (obj1 > max) max = obj1;
+if (obj2 > max) max = obj2;
+if (obj3 > max) max = obj3;
+if (obj4 > max) max = obj4;
+if (obj5 > max) max = obj5;
